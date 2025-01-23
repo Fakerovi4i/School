1f313b28136b2885bf8eb97d066f2ef95dcc5dc6
Fakerovi4i <fakerovi4i@gmail.com>
2025-01-23 18:17:08 +0300

    Update 3. Глубокое копирование.py

--- 8-Функция - Рекурсия/Практика/3. Глубокое копирование.py
+++ 8-Функция - Рекурсия/Практика/3. Глубокое копирование.py
@@ -1,38 +1,38 @@
 import copy
-
 site = {
 'html': {
 'head': {
-'title': 'Куплю/продам телефон недорого'
+'title': 'Куплю/продам {} недорого'
 },
 'body': {
-'h2': 'У нас самая низкая цена на телефоны',
+'h2': 'У нас самая низкая цена на {}',
 'div': 'Купить',
 'p': "Продать"
 }
 }
 }
-def find_key(dict_1, key):
+def change_key(dict_1, key, new_prod="НАЗВАНИЕ"):
 	if key in dict_1:
-		dict_1[key] = new_prod
+		dict_1[key] = dict_1[key].format(new_prod)
 	for i_value in dict_1.values():
 		if isinstance(i_value, dict):
-			result = find_key(i_value, key)
-			if result:
-				break
-	else:
-		result = None
-
-	return result
-
-def make_site():
-	sites = []
-	new_site = find_key(copy.deepcopy(site), 'title')
-	sites.append(new_site)
-
-
-
-
-quant_product = 2
-new_product = ["Samsung", "Iphone"]
-make_site(site, new_product, quant_product)
+			change_key(i_value, key, new_prod)
+	
+def make_site(site, quant_product):
+  sites = []
+  names_sites = []
+  for ind_x in range(quant_product):
+    new_product = input("Ваедите название продукта: ")
+    keys = ['title',"h2"]
+    new_site = copy.deepcopy(site)
+    for i_keys in range(2):
+      change_key(new_site, keys[i_keys], new_prod=new_product)
+    sites.append(new_site)
+    names_sites.append(new_product)
+    
+    for i_site, site_dict in zip(names_sites, sites):
+      print("Сайт для {}:".format(i_site))
+      print(site_dict)
+	
+quant_product = int(input("Сколько сайтов?: "))
+make_site(site, quant_product)
